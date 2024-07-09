933fcd375334d27050ff8e0e081210c846941adb
JJyeob <b01031722606@gmail.com>
2024-07-09 13:19:43 +0900

    최종변경사항

--- news.py
+++ news.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file, jsonify
+from flask import Flask, render_template, request
 import urllib.request
 import datetime, re, json, os, smtplib
 from openpyxl import Workbook
